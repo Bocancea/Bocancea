cf2a7626d49b0aa64a660299aa4d16ac11c48ed0
BocanceaM <mihailbocancea@gmail.com>
2022-05-26 22:18:52 +0300

    mesaj privat

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
-     String[] words = text.split(" ");
-for (int i=0; i< words.length; i++)
- System.out.println(words[i].charAt(0));
-
-
+        String[] words = text.split(" ");
+        for (int i = 0; i < words.length; i++)
+            System.out.println(words[i].charAt(0));
     }
 
 }
