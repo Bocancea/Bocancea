e24188067b21d105e2b6d191df80fc3ea6e988db
BocanceaM <mihailbocancea@gmail.com>
2022-05-26 19:08:58 +0300

    7.4 Name initials

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
-        String max = "";
      String[] words = text.split(" ");
 for (int i=0; i< words.length; i++)
- if (words[i].length()>max.length()) max=words[i];
-        System.out.println(max);
+ System.out.print(words[i].charAt(0));
+
 
     }
 
