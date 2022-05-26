ff4b34a3140086c673379a54ce80654cbc87a9b4
BocanceaM <mihailbocancea@gmail.com>
2022-05-26 23:18:01 +0300

    cleane project

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
-        int l1 = scanner.nextInt();
-        int l2 = scanner.nextInt();
-        System.out.println(text.substring(l1, l2 + 1));
+        // please write your code here
     }
 
 }
